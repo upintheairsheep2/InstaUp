5d7f43970f5e864d59d9edbbd3049d1501f9bc16
Coloradohusky <45430066+Coloradohusky@users.noreply.github.com>
2020-05-29 12:16:30 -0400

    Update instaup.py
    -Added basic error checking to deal with potential 503 slowdown errors

--- instaup.py
+++ instaup.py
@@ -3,6 +3,7 @@ import datetime
 from internetarchive import upload
 from internetarchive import get_item
 from os import rmdir
+from os import path
 
 currentTime = datetime.datetime.now(datetime.timezone.utc)
 fixedTime = str(currentTime.strftime("%Y%m%d%H%M%S"))
@@ -13,5 +14,9 @@ profile = instaloader.Profile.from_username(loader.context, username)
 loader.download_profiles([profile], igtv=True, highlights=True, stories=True)
 fullName = username + '-' + fixedTime
 item = get_item('instagram-' + fullName)
-item.upload('./' + fullName + '/', verbose=True, delete=True, metadata=dict(collection='opensource_media', subject='instagram', creator=username, title='Instagram Profile: ' + username, originalurl='https://www.instagram.com/' + username, scanner='InstaUp 1.0'))
+try:
+    item.upload('./' + fullName + '/', verbose=True, delete=True, metadata=dict(collection='opensource_media', subject='instagram', creator=username, title='Instagram Profile: ' + username, originalurl='https://www.instagram.com/' + username, scanner='InstaUp 1.0'), retries=9001, retries_sleep=60)
+except:
+    print ("An error occurred, trying again.")
+    item.upload('./' + fullName + '/', verbose=True, delete=True, metadata=dict(collection='opensource_media', subject='instagram', creator=username, title='Instagram Profile: ' + username, originalurl='https://www.instagram.com/' + username, scanner='InstaUp 1.0'), retries=9001, retries_sleep=60)
 rmdir(fullName)
