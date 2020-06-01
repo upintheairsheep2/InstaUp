a9cdb7ebd7cb3ac451fd06560a491276976dcb67
Coloradohusky <45430066+Coloradohusky@users.noreply.github.com>
2020-05-31 21:20:02 -0400

    Update instaup.py
    Added prompt for deletion of files, made program upload to 'instagram-<USERNAME>', removed all time references

--- instaup.py
+++ instaup.py
@@ -1,16 +1,11 @@
 import instaloader
-import datetime
 from internetarchive import upload
 from internetarchive import get_item
 from os import rmdir
 from os import path
 
-def getTime():
-    currentTime = datetime.datetime.now(datetime.timezone.utc)
-    return str(currentTime.strftime("%Y%m%d%H%M%S"))
-
-def downloadUser(username, respectPrivacy, time):
-    loader = instaloader.Instaloader(dirname_pattern='{profile}-' + time)
+def downloadUser(username, respectPrivacy):
+    loader = instaloader.Instaloader(dirname_pattern='{profile}')
     loader.load_session_from_file(<YOUR INSTAGRAM USERNAME>)
     profile = instaloader.Profile.from_username(loader.context, username)
     if ((profile.is_private == True) and (respectPrivacy == ('yes' or 'Yes' or 'Y' or 'y'))):
@@ -19,19 +14,26 @@ def downloadUser(username, respectPrivacy, time):
     loader.download_profiles([profile], igtv=True, highlights=True, stories=True)
     return True
 
-def uploadUser(username, time):
-    fullName = username + '-' + time
-    item = get_item('instagram-' + fullName)
+def uploadUser(username, shouldDelete):
+    if (shouldDelete == ('yes' or 'Yes' or 'Y' or 'y')):
+        deletionStatus = True
+    else:
+        deletionStatus = False
+    item = get_item('instagram-' + username)
     try:
-        item.upload('./' + fullName + '/', verbose=True, delete=True, metadata=dict(collection='opensource_media', subject='instagram', creator=username, title='Instagram Profile: ' + username, originalurl='https://www.instagram.com/' + username, scanner='InstaUp 1.0'), retries=9001, retries_sleep=60)
+        item.upload('./' + username + '/', verbose=True, checksum=True, delete=deletionStatus, metadata=dict(collection='opensource_media', subject='instagram', creator=username, title='Instagram Profile: ' + username, originalurl='https://www.instagram.com/' + username, scanner='InstaUp 1.0'), retries=9001, retries_sleep=60)
     except:
         print ("An error occurred, trying again.")
-        item.upload('./' + fullName + '/', verbose=True, delete=True, metadata=dict(collection='opensource_media', subject='instagram', creator=username, title='Instagram Profile: ' + username, originalurl='https://www.instagram.com/' + username, scanner='InstaUp 1.0'), retries=9001, retries_sleep=60)
-    rmdir(fullName)
+        item.upload('./' + username + '/', verbose=True, checksum=True, delete=deletionStatus, metadata=dict(collection='opensource_media', subject='instagram', creator=username, title='Instagram Profile: ' + username, originalurl='https://www.instagram.com/' + username, scanner='InstaUp 1.0'), retries=9001, retries_sleep=60)
+    try:
+        rmdir(username)
+        print ("Deleted folder " + username)
+    except:
+        print ("Didn't delete " + username)
 
-fullTime = getTime()
 user = input('Enter in an Instagram username: ')
-privacy = input('Should I respect people\'s privacy? ')
-didItWork = downloadUser(user, privacy, fullTime)
+privacy = input('Should I respect the user\'s privacy? ')
+deletion = input('Do you want to delete files when done? ')
+didItWork = downloadUser(user, privacy)
 if (didItWork == True):
-    uploadUser(user, fullTime)
+    uploadUser(user, deletion)
