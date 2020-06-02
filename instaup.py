3c140cd99cd1460270dbc5a4f316343d42195ebd
Coloradohusky <45430066+Coloradohusky@users.noreply.github.com>
2020-06-02 17:46:42 -0400

    Update instaup.py
    made it work from the command line

--- instaup.py
+++ instaup.py
@@ -3,22 +3,19 @@ from internetarchive import upload
 from internetarchive import get_item
 from os import rmdir
 from os import path
+import argparse
 
 def downloadUser(username, respectPrivacy):
     loader = instaloader.Instaloader(dirname_pattern='{profile}', compress_json=False)
     loader.load_session_from_file(<YOUR INSTAGRAM USERNAME>)
     profile = instaloader.Profile.from_username(loader.context, username)
-    if ((profile.is_private == True) and (respectPrivacy == ('yes' or 'Yes' or 'Y' or 'y'))):
+    if ((profile.is_private == True) and (respectPrivacy == True)):
         print (username + " is a private profile. Exiting...")
         return False
     loader.download_profiles([profile], igtv=True, highlights=True, stories=True)
     return True
 
-def uploadUser(username, shouldDelete):
-    if (shouldDelete == ('yes' or 'Yes' or 'Y' or 'y')):
-        deletionStatus = True
-    else:
-        deletionStatus = False
+def uploadUser(username, deletionStatus):
     item = get_item('instagram-' + username)
     try:
         item.upload('./' + username + '/', verbose=True, checksum=True, delete=deletionStatus, metadata=dict(collection='opensource_media', subject='instagram', creator=username, title='Instagram Profile: ' + username, originalurl='https://www.instagram.com/' + username, scanner='InstaUp 1.0'), retries=9001, retries_sleep=60)
@@ -28,12 +25,18 @@ def uploadUser(username, shouldDelete):
     try:
         rmdir(username)
         print ("Deleted folder " + username)
+        return
     except:
-        print ("Didn't delete " + username)
+        return
 
-user = input('Enter in an Instagram username: ')
-privacy = input('Should I respect the user\'s privacy? ')
-deletion = input('Do you want to delete files when done? ')
-didItWork = downloadUser(user, privacy)
+parser = argparse.ArgumentParser(description='An auto downloader and uploader for Instagram profiles.')
+parser.add_argument('user')
+parser.add_argument('--privacy', action='store_true', help="check the user's privacy settings")
+parser.add_argument('--delete', action='store_true', help="delete files when done")
+args = parser.parse_args()
+username = args.user
+privacy = args.privacy
+delete = args.delete
+didItWork = downloadUser(username, privacy)
 if (didItWork == True):
-    uploadUser(user, deletion)
+    uploadUser(username, delete)
