4f7e276ef16046e04fc05d954e7368b8180c094f
Coloradohusky <45430066+Coloradohusky@users.noreply.github.com>
2020-06-06 18:06:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(
