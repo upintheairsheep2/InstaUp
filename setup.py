29705aa83932472e06b456ce2531ed2705e93c2a
Coloradohusky <45430066+Coloradohusky@users.noreply.github.com>
2020-06-06 17:35:28 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='instaup',
+    version='1.0',
+    description='An auto downloader and uploader for Instagram profiles.',
+    long_description=long_description,
+    url='https://github.com/Coloradohusky/InstaUp',
+    author='Coloradohusky',
+    py_modules=['instaup'],
+    python_requires='>=3.5, <4',
+    install_requires=['internetarchive>=1.9.3', 'instaloader>=4.4.3']
+)
